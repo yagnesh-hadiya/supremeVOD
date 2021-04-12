a52dcdd2e95b8ee4b2368fea995cdd17b69aa866
Jigar Dhameliya <jp92370@gmail.com>
2021-04-12 16:53:14 +0530

    init

--- App.js
+++ App.js
@@ -1,87 +1,26 @@
-import React from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  ImageBackground,
-  Dimensions,
-  TextInput,
-  Platform,
-  KeyboardAvoidingView,
-} from 'react-native';
-const {height, width} = Dimensions.get('window');
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const App = () => {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <ImageBackground 
-        source={require('./src/images/h2.png')}
-        style={styles.image}>
-        <View style={styles.loginCard}>
-          <Text style={styles.loginText}>Login</Text>
-          <KeyboardAvoidingView style={styles.textInputWrapper}>
-            <TextInput
-              style={styles.textInput}
-              placeholder="Enter your access code"
-              placeholderTextColor='#fff'
-            />
-          </KeyboardAvoidingView>
-          <View style={styles.submitButton}>
-            <Text style={styles.submitText}> Submit</Text>
-          </View>
-        </View>
-      </ImageBackground>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
-};
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  image: {
-    width: width,
-    height: height,
-    // opacity: 0.9,
-  },
-  loginCard: {
-    marginHorizontal: 30,
-    backgroundColor: 'rgba( 0, 0, 0,0.9)',
-    alignItems: 'center',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    height: 350,
-  },
-  loginText: {
-    color: 'white',
-    fontSize: 30,
-    fontWeight: 'bold',
-    marginVertical: 35,
-  },
-  textInputWrapper: {
-    backgroundColor: '#6D6D6D',
-    padding: Platform.OS === 'ios' ? 15 : 0,
-    borderRadius: 10,
-    width: 230,
-    height: 52,
-  },
-  textInput: {
-    color: '#fff',
-    fontSize: 18,
-  },
-  submitButton: {
-    backgroundColor: '#FF0000',
-    padding: 15,
-    borderRadius: 10,
-    width: 230,
-    marginTop: 60,
-  },
-  submitText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 export default App;
